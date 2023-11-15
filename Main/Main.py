7eeca6ad225ed417b3d7c1a2ab2cd3e78cf6e03f
Luka Perich <lperich29@gmail.com>
2023-11-15 16:17:59 -0300

    Acutalizo main y README

--- Main/Main.py
+++ Main/Main.py
@@ -1,18 +1,47 @@
-#Primero llenamos la matriz:
-#(Este paso todavía tiene que ser desarrollado)
+#Usamos esta funcion para darle dimension y llenar la matriz 
+def llenarMatriz():
+    dimension=int(input("Ingrese la dimesion de la matriz: "))
+    matriz = [[0] * dimension for _ in range(dimension)]
 
-#Evaluamos las condiciones, primo lo hacemos de manera horizontal
-coindencia=0
+    for fila in range(dimension):
+        for columna in range(dimension):
+            while True: 
+                letra = input(f"Ingrese la letra de la cadena numero: [{columna}]:").upper()
+                if (letra=="A" or letra=="T" or letra=="C" or letra=="G" ):
+                    matriz[fila][columna] = letra
+                    break
+                else:
+                        print("El valor ingresado no es correcto intente nuevamente")
+    return matriz
 
-for fila in Matriz: 
-    for i in range(len(fila)-4): 
-        if (fila[i]==fila[i+1] and fila[i+1]==fila[i+2] and fila[i+2]==fila[i+4]) or (fila[i+1]==fila[i+2] and fila[i+2]==fila[i+3] and fila[i+3]==fila[i+4]) or (fila[i+2]==fila[i+3] and fila[i+3]==fila[i+4] and fila[i+4]==fila[i+5]): 
-            coindencia=coindencia+1
-        
+def AnalizarCondcion(matriz1):
+    coincidencia = 0
+    # Empezamos a recorrer la matriz y evaluarla
+    for i in range(len(matriz1)):
+        for j in range(len(matriz1[0]) - 3):
+            # Horizontal
+            if len(set(matriz1[i][j : j + 4])) == 1:
+                coincidencia += 1
 
-#Determinamos la condicion de la persona: 
+            # Vertical
+            if len(set(matriz1[i][j] for i in range(i, i + 4))) == 1:
+                coincidencia += 1
 
-if coindencia>=2: 
-    print("La persona es: Mutante")
-else: 
-    print("La persona no es: Muntante")
+    for i in range(len(matriz1) - 3):
+        for j in range(len(matriz1[0]) - 3):
+            
+            if len(set(matriz1[i + k][j + k] for k in range(4))) == 1:
+                coincidencia += 1
+
+            
+            if len(set(matriz1[i + k][j + 3 - k] for k in range(4))) == 1:
+                coincidencia += 1
+    #Retornamos la condicion de la persona: 
+    if coincidencia >= 2:
+        return print("La persona es: Mutante")
+    else:
+        return print("La persona no es: Mutante")
+
+#Llamamos a las funciones
+Matriz= llenarMatriz()
+Condicon=AnalizarCondcion(Matriz)
